dcbcd67846757b261a99d9254c0fb3a3ab0ddcd7
Katze937 <145044364+Katze937@users.noreply.github.com>
2025-03-15 12:24:32 -0400

    add Login, Register, Home pages
    Last time can't successfully add the src file

---
+++ src/pages/Home.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import jwtDecode from "jwt-decode";
+
+const Home = () => {
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      const decoded = jwtDecode(token);
+      setUser(decoded);
+    } else {
+      navigate("/login"); 
+    }
+  }, [navigate]);
+
+  return (
+    <div>
+      <h2>Welcometo E-Mart</h2>
+      {user && <p>Hi, {user.email}</p>}
+      <button onClick={() => { localStorage.removeItem("token"); navigate("/login"); }}>
+        Logout
+      </button>
+    </div>
+  );
+};
+
+export default Home;
